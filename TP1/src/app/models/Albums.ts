891b00ac1e44b40bcc883063f44085aaaf6ac6e9
6224745 <6224745@cegepmontpetit.ca>
2024-09-18 10:59:14 -0400

    Creation class Artist

--- TP1/src/app/models/Albums.ts
+++ TP1/src/app/models/Albums.ts
@@ -1,6 +1,7 @@
 export class Album{
     constructor(
         public name : string,
-        public NomArtiste : string
+        public NomArtiste : string,
+        public NumChansons : number
     ){}
 }
